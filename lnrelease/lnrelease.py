05e92b90390415386a2c9e7757303a1c9ab0aa68
LNRelease <139043193+LNRelease@users.noreply.github.com>
2023-10-23 09:42:32 +1100

    watchdog

--- lnrelease/lnrelease.py
+++ lnrelease/lnrelease.py
@@ -1,9 +1,12 @@
+from faulthandler import dump_traceback_later
+
 import parse
 import scrape
 import write
 
 
 def main():
+    dump_traceback_later(60*60*2, exit=True)
     scrape.main()
     parse.main()
     write.main()
