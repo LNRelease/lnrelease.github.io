f2da7a7ecef7015f404174cdff620a99c8d64b3e
LNRelease <139043193+LNRelease@users.noreply.github.com>
2024-08-11 15:30:43 +1000

    improve yp parsing

--- lnrelease/publisher/yen_press.py
+++ lnrelease/publisher/yen_press.py
@@ -1,6 +1,6 @@
 from utils import Book, Info, Series
 
-from . import check, copy, guess, omnibus, one, secondary, short, standard
+from . import NUMBER, check, copy, guess, omnibus, one, part, secondary, short, standard
 
 NAME = 'Yen Press'
 
@@ -25,10 +25,19 @@ def parse(series: Series, info: dict[str, list[Info]],
     standard(series, info, books)
     omnibus(series, info, books)
     short(series, info, books)
-    if main_books.count(None) < size - 1:
+    todo = [i for i, book in enumerate(main_books) if not book]
+    if len(todo) == 0:
+        pass
+    elif all((' Volume ' in main_info[i].title
+              or ' Vol. ' in main_info[i].title)
+             and len(NUMBER.findall(main_info[i].title)) > 1
+             for i in todo):
+        part(series, info, books)
+    elif len(todo) < size - 1:
         one(series, info, books)
     else:
         secondary(series, info, links, books)
+        short(series, info, books)
         guess(series, info, books)
 
     copy(series, info, books)
